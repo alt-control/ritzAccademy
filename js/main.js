15192f4a247301d5fa29c16e1dba1ec7181a1a00
fogez <410dheeraj@gmail.com>
2018-06-23 14:10:39 +0530

    maps update
    maps update

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 (function($) {
 	"use strict"
-	
+
 	// Preloader
 	$(window).on('load', function() {
 		$("#preloader").delay(600).fadeOut();
@@ -10,5 +10,5 @@
 	$('.navbar-toggle').on('click',function(){
 		$('#header').toggleClass('nav-collapse')
 	});
-	
+
 })(jQuery);
